a3345757e827093985c5924efd05a910af7985ce
Peter Novotnak <peter@cyrusbio.com>
2016-03-22 17:26:14 -0700

    removed Address field from client tests

--- client_test.go
+++ client_test.go
@@ -10,16 +10,12 @@ import (
 var client = Client{}
 
 func TestClientCanSendMessage(t *testing.T) {
-	client.Send("garbage!")
+	client.Broadcast("garbage!")
 }
 
 func TestMain(m *testing.M) {
 	str, _ := SecureRandomAlphaString(10)
 	client = Client{
-		Address: &net.UDPAddr{
-			IP:   net.IPv4(0, 0, 0, 0),
-			Port: 19091,
-		},
 		UID: fmt.Sprint("test-client-", str),
 	}
 	os.Exit(m.Run())
